fa61ad23badb86c5ce58cbe719891ad29e3dd7c5
Matiss Janis Aboltins <matiss@mja.lv>
2018-12-29 21:58:01 +0200

    Cleanup resolvers

--- src/apollo/Resolvers.js
+++ src/apollo/Resolvers.js
@@ -1,14 +1,12 @@
 export const resolvers = {
   Mutation: {
     setSelectedDates: (_, variables, { cache }) => {
-      // @todo: can this be improved?
       cache.writeData({
         data: {
           start_date: variables.start_date,
           end_date: variables.end_date,
         },
       });
-      return null;
     },
   },
 };
